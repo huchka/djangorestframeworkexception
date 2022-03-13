f9c2e412e480b942ae0126beb4bc27219020dfcb
Khureltulga Dashdavaa <huchka@me.com>
2022-03-13 19:58:24 +0900

    note api can create now.

--- api/views.py
+++ api/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django_filters import rest_framework
-from rest_framework import status, filters
-from rest_framework.generics import ListAPIView
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -19,7 +19,7 @@ class HelloWorld(APIView):
         return Response({"message": "Hello World!!!"}, status.HTTP_200_OK)
 
 
-class NoteList(ListAPIView):
+class NoteList(ListCreateAPIView):
 
     serializer_class = NoteSerializer
     queryset = Note.objects.all()
